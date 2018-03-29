26246ad0def29d5b3854c50d1fe337c48e5ea2b3
Denis <35633718+DenisGubeev@users.noreply.github.com>
2018-03-29 16:34:52 +0300

    Create Smartphone.php

---
+++ classes/Smartphone.php
@@ -0,0 +1,12 @@
+<?php
+interface SmartphoneInterface
+{
+    public function areYouRich();
+}
+class Smartphone extends Product implements SmartphoneInterface
+{
+    protected $color;
+    protected $model;
+    use SmartphoneTrait;
+}
+?>
