81b6782815521ebf93f38155565eefc800d3ece7
Denis <35633718+DenisGubeev@users.noreply.github.com>
2018-03-29 16:36:40 +0300

    Update Product.php

--- classes/Product.php
+++ classes/Product.php
@@ -1,4 +1,16 @@
 <?php
+
+interface ProductInterface
+{
+    public function isFood();
+    public function discount();
+    public function deliveryPrice();
+    public function totalPrice();
+    public function getProperty($property);
+    public function setProperty($property, $value);
+    public function setPropertyWithReturn($property, $value);
+}
+
 abstract class Product implements ProductInterface
 {
     protected $title;
