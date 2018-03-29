8eee385b69eee0d54e1c6849f8ecc9f12399200f
Denis <35633718+DenisGubeev@users.noreply.github.com>
2018-03-29 16:48:38 +0300

    Update Smartphone.php

--- classes/Smartphone.php
+++ classes/Smartphone.php
@@ -3,6 +3,17 @@ interface SmartphoneInterface
 {
     public function areYouRich();
 }
+
+trait SmartphoneTrait
+{
+    // кхм-кхм
+    public function areYouRich()
+    {
+        if ($this->model == 'android') return false;
+        return true;
+    }
+}
+
 class Smartphone extends Product implements SmartphoneInterface
 {
     protected $color;
