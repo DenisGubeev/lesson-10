e523021c8649924d7f981f9b05adbaca05935b31
Denis <35633718+DenisGubeev@users.noreply.github.com>
2018-03-29 16:51:01 +0300

    Update Product.php

--- classes/Product.php
+++ classes/Product.php
@@ -17,7 +17,7 @@ abstract class Product implements ProductInterface
     protected $price;
     protected $type;
     protected $weight;
-    // Создаем объект
+   
     public function __construct($title, $price, $type, $weight)
     {
         $this->title = $title;
@@ -25,14 +25,7 @@ abstract class Product implements ProductInterface
         $this->type = $type;
         $this->weight = $weight;
     }
-    // Размер скидки
-    public function discount()
-    {
-        $isFood = $this->isFood();
-        $foodWeight = $this->weight;
-        if (($isFood && $foodWeight > 10) || (!$isFood)) return $this->price*0.1;
-        return 0;
-    }
+    
     public function isFood()
     {
         return $this->type == 'food';
@@ -49,19 +42,4 @@ abstract class Product implements ProductInterface
     {
         return $this->price - $this->discount() + $this->deliveryPrice();
     }
-    // Универсальный геттер
-    public function getProperty($property)
-    {
-        return $this->$property;
-    }
-    // Универсальный сеттер
-    public function setProperty($property, $value) {
-        $this->$property = $value;
-    }
-    // Универсальный сеттер с return'ом
-    public function setPropertyWithReturn($property, $value) {
-        $this->$property = $value;
-        return $this;
-    }
-}
 ?>
